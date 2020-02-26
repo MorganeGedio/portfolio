946e3c86aa6f1f74929095a13261f9d63f043fe5
Morgane Gedio <morgane.gedio@gmail.com>
2020-02-26 12:41:10 +0100

    add Thumbnail component

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import Thumbnail from './Thumbnail.js';
+import './App.css';
+import './Project.css';
 
 const Home = () => (
     <div>
-        <h1>WELCOME !</h1>
+        <h1>Bonjour !</h1>
+          <h2>Projects</h2>
+            <div className="project-container">
+              <Thumbnail
+              path="https://cocktail-hour.herokuapp.com/"
+              image="https://res.cloudinary.com/frijolyfrailejon/image/upload/v1582715587/Portfolio/cocktail_hour-index_oy8rr7.png"
+              title="Cocktail Hour"
+              category="Le Wagon"
+              description="Coming soon ..."
+              />
+
+               <Thumbnail
+              path="https://whistle-island.herokuapp.com/"
+              image="https://res.cloudinary.com/frijolyfrailejon/image/upload/v1582715588/Portfolio/whistle_island_home_dsds6e.png"
+              title="Whistle Island"
+              category="Le Wagon"
+              description="Coming soon ..."
+              />
+
+               <Thumbnail
+              path="https://www.whistle-app.com/"
+              image="https://res.cloudinary.com/frijolyfrailejon/image/upload/v1582717019/Portfolio/whistle_laptop_rscg8k.png"
+              title="Whistle"
+              category="Le Wagon"
+              description="Coming soon ..."
+              />
+            </div>
     </div>
 );
 
---
+++ src/Thumbnail.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import './Project.css';
+
+function Thumbnail(props) {
+  return (
+    <div className="Project">
+        <div className="project-image">
+          <a href={props.path} target="_blank">
+            <img src={props.image} alt="Mister Cocktail"/>
+          </a>
+        </div>
+        <div className="project-title">{props.title}</div>
+        <div className="project-category">{props.category}</div>
+        <p className="project-description">{props.description}</p>
+    </div>
+  );
+}
+
+export default Thumbnail;
