f5ae83dc512222c5d624956705f573a11f8e8123
Morgane Gedio <morgane.gedio@gmail.com>
2020-03-03 17:40:28 +0100

    Updates

---
+++ precache-manifest.d1b8012acb511ee8400f14b8f869b975.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ddf8689a07b58b04d0135f3efdb56e6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7c81bd08d5203aafd0a4",
+    "url": "/portfolio/static/css/main.6d087f1e.chunk.css"
+  },
+  {
+    "revision": "8a819cf90b45037cc66d",
+    "url": "/portfolio/static/js/2.8e433be3.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.8e433be3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c81bd08d5203aafd0a4",
+    "url": "/portfolio/static/js/main.eb361858.chunk.js"
+  },
+  {
+    "revision": "1a95985286676740b556",
+    "url": "/portfolio/static/js/runtime-main.f6eb50f0.js"
+  },
+  {
+    "revision": "d23b20a114ac0ce540bafbce73059d91",
+    "url": "/portfolio/static/media/logo_portfolio.d23b20a1.png"
+  }
+]);
