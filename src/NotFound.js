8d499042fbee70d417b69a4c854eb0a8767a9864
Morgane Gedio <morgane.gedio@gmail.com>
2020-03-05 16:03:43 +0100

    modify style of not found page

--- src/NotFound.js
+++ src/NotFound.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NotFound = () => (
     <div>
-        <h2>You're lost !</h2>
-        <img src="https://images.unsplash.com/photo-1513797839453-8d1e735369ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80" alt="Lost"/>
+        <h2 className="not-found">You're lost !</h2>
+        <img src="https://images.unsplash.com/photo-1513797839453-8d1e735369ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80" alt="Lost" className="not-found"/>
     </div>
 );
 
